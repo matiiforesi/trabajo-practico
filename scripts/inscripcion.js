906ce05f2b0cc070e959c711f814324adf5d71b0
FelipeSguerzo <fsguerzo231@alumno.unlam.edu.ar>
2024-10-24 20:50:17 -0300

    Agregue la funcionalidad de ingresar y eliminar personas en el formulario de inscripcion

---
+++ scripts/inscripcion.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('.contenedor-fields');
+const addPersonButton = document.getElementById('add-person');
+addPersonButton.addEventListener('click', addPerson);
+document.addEventListener('DOMContentLoaded',addPerson);
+
+function addPerson(event){
+    const fields = document.querySelector('.fields');
+    const field = document.createElement('div');
+    field.classList.add('field');
+
+    const nombre = document.createElement('input');
+    nombre.type = 'text';
+    nombre.placeholder = 'Nombre';
+
+    const apellido = document.createElement('input');
+    apellido.type = 'text';
+    apellido.placeholder = 'Apellido';
+
+    const numero = document.createElement('input');
+    numero.type = 'number';
+    numero.placeholder = 'DNI';
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.id = 'delete';
+    button.classList = 'deletePerson'
+    button.onclick = function (){
+        if (fields.querySelectorAll('.field').length > 1) {
+            field.remove();
+        } else {
+            alert('Debe haber al menos un campo.');
+        }
+    }
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = '/assets/circulo-negativo.png'
+    deleteIcon.alt = 'delete';
+    
+    button.appendChild(deleteIcon);
+
+    field.appendChild(nombre);
+    field.appendChild(apellido);
+    field.appendChild(numero);
+    field.appendChild(button);
+
+    fields.appendChild(field);
+}
